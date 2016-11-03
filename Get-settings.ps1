f289d8d4ff6828b3419863b522e7f21d8beeb541
Chris Crow <chris@ivoxy.com>
2016-11-03 15:38:09 -0700

    Initial scripting swag, and stuff...

---
+++ Get-settings.ps1
@@ -0,0 +1,15 @@
+﻿
+
+#Quick and dirty script to get cluster settings from a vCenter server that RVTools misses
+
+$viserver = "seadc1vc01.ldthost.pvt"
+
+#modify this line!!!
+Connect-VIServer -server $viserver
+
+get-cluster | format-list -property * |  out-file -FilePath "C:\git\PowerCLI-Data-Collection\results.txt"
+
+Get-AdvancedSetting -Entity $viserver  | format-wide -property Id |  out-file -Append -FilePath "C:\git\PowerCLI-Data-Collection\results.txt"
+
+
+Disconnect-VIServer
